6e24c04b09d80ef783ddbeb671cecd80b3e5d2bc
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2024-11-12 17:56:13 +0700

    Added la so kham thien

--- lasotuvi_kk/lasotuvi_kk/settings.py
+++ lasotuvi_kk/lasotuvi_kk/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,9 +26,7 @@ SECRET_KEY = "django-insecure-yzm%nfri+u9bh%gx038m9w27k)fb6x+p^akmwlt8b3v-ip^!l6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '327e-2405-4802-1cb0-d960-ecdc-632d-1e8e-f9d1.ngrok-free.app']
-
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'bluegreen.id.vn', 'bluegreen.id.vn/lasotuvi']
 
 # Application definition
 
@@ -118,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
