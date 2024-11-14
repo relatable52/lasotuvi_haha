b7c4ff1e7ae1c1c640c0c320524b44b2d323979c
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2024-11-14 07:01:37 +0700

    final

--- lasotuvi_kk/xemlaso/static/drawing.js
+++ lasotuvi_kk/xemlaso/static/drawing.js
@@ -20,14 +20,6 @@ function velaso(laso, loaiLaSo = 0){
         .attr("viewBox", `0 0 ${width} ${height}`)
         .attr("id", "chart_svg")
         .attr("preserveAspectRatio", "xMinYMin meet");
-
-    var printable = d3.select("#printable")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", `0 0 ${width} ${height}`)
-    .append("use")
-    .attr("xlink:href", "#chart_svg");
    
     const svg = chart_svg.append("g");
 
@@ -74,19 +66,6 @@ function velaso(laso, loaiLaSo = 0){
             .attr("stroke", "black");
     }
 
-    svg.append("defs")
-        .append("marker")
-        .attr("id", "arrowhead")
-        .attr("viewBox", "0 -5 10 10")
-        .attr("refX", 10)  // Controls position of the arrowhead along the path
-        .attr("refY", 0)
-        .attr("markerWidth", 6)
-        .attr("markerHeight", 6)
-        .attr("orient", "auto")  // Keeps arrowhead pointed along the path direction
-        .append("path")
-        .attr("d", "M0,-5L10,0L0,5")
-        .attr("fill", "red");
-
     function addText(text, x, y, style="", justify="middle"){
         svg.append("text")
         .attr("class", style)
@@ -106,11 +85,22 @@ function velaso(laso, loaiLaSo = 0){
             .attr("y2", y2) // Ending point y
             .attr("stroke", "black")
             .attr("stroke-width", 1)
-            .attr("marker-end", "url(#arrowhead)")
             .attr("class", style);
 
         const offset = 20; // Distance from the arrow tip
         const angle = Math.atan2(y2 - y1, x2 - x1);  // Angle of the line
+
+        // Calculate triangle points based on the angle
+        const arrowSize = 7;
+        const arrowX1 = x2 - arrowSize * Math.cos(angle - Math.PI / 6);
+        const arrowY1 = y2 - arrowSize * Math.sin(angle - Math.PI / 6);
+        const arrowX2 = x2 - arrowSize * Math.cos(angle + Math.PI / 6);
+        const arrowY2 = y2 - arrowSize * Math.sin(angle + Math.PI / 6);
+
+        // Append the triangle as a polygon
+        svg.append("polygon")
+            .attr("points", `${x2},${y2} ${arrowX1},${arrowY1} ${arrowX2},${arrowY2}`)
+            .attr("class", style);
 
         const labelX = x2 + offset * Math.cos(angle+3*Math.PI/6);
         const labelY = y2 + offset * Math.sin(angle+3*Math.PI/6);
@@ -458,17 +448,17 @@ function velaso(laso, loaiLaSo = 0){
         addText(thienBanText[i][3], left2 + 4*stepx1, up1 + thienBanText[i][1]*stepy1, "thienban blue bold", "start");
     }
 
-    d3.xml("static/converted_image.svg")  // Replace with the path to your external SVG file
+    d3.xml("static/bitmap1.svg")  // Replace with the path to your external SVG file
     .then(function(data) {
         const externalSvg = data.documentElement;
 
         // Option 1: Change the location using x and y attributes
         d3.select(externalSvg)
-        .attr("x", 3*width/4 - 75)  // Move the external SVG to x=100
-        .attr("y", 3*height/4 - 75)  // Move the external SVG to y=100
-        .attr("width", 70)
-        .attr("height", 70)
-        .attr("viewBox", "0 0 70 70");
+        .attr("x", 3*width/4 - 127)  // Move the external SVG to x=100
+        .attr("y", 3*height/4 - 127)  // Move the external SVG to y=100
+        .attr("width", 125)
+        .attr("height", 125)
+        .attr("preserveAspectRatio", "xMidYMid meet");
 
         // Option 2: Use transform to move (this is more flexible)
         // d3.select(externalSvg)
@@ -503,6 +493,9 @@ function velaso(laso, loaiLaSo = 0){
         const translateY = (height * (1 - scaleFactor)) / 2;
         svg.style("transform", `translate(${translateX}px, ${translateY}px) scale(${scaleFactor})`);
     }
+
+    document.getElementById("printable").innerHTML = document.getElementById("chart").innerHTML;
+    d3.select("#printable svg").attr("width", width).attr("height", height);
 }
 
 function zoomIn(){
@@ -559,20 +552,26 @@ function velaso3(){
 
 function download(filename = 'downloaded_image.png'){
     const svgElement = d3.select("#chart svg").node();
-    
-    var svgString = preprocess(svgElement).source;
-    
+    const svgString = preprocess(svgElement).source;
     const img = new Image();
     const svgBlob = new Blob([svgString], { type: 'image/svg+xml;charset=utf-8' });
     const url = URL.createObjectURL(svgBlob);
 
     img.onload = () => {
-        // Draw SVG image onto a canvas
+        const scaleFactor = 2; // Change this factor to make the image larger or smaller
+
+        // Create a canvas with scaled dimensions
         const canvas = document.createElement('canvas');
-        canvas.width = svgElement.clientWidth;
-        canvas.height = svgElement.clientHeight;
+        canvas.width = svgElement.clientWidth * scaleFactor;
+        canvas.height = svgElement.clientHeight * scaleFactor;
         const context = canvas.getContext('2d');
-        context.drawImage(img, 0, 0);
+
+        // Fill the canvas with a white background
+        context.fillStyle = "white";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        // Draw the image at the new scaled size
+        context.drawImage(img, 0, 0, canvas.width, canvas.height);
 
         // Create a PNG download link
         const pngUrl = canvas.toDataURL('image/png');
