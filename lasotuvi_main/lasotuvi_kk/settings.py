60a2651619a1bdda5ae0d7aae779d4b01dbb693c
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2024-11-23 01:13:16 +0700

    final

--- lasotuvi_main/lasotuvi_kk/settings.py
+++ lasotuvi_main/lasotuvi_kk/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-yzm%nfri+u9bh%gx038m9w27k)fb6x+p^akmwlt8b3v-ip^!l6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'khamthientuhoa.com.vn']
 
